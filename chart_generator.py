5f70398ac96f20ef1285e9169e24093da3ea458f
OutcastOuO <k0403412411@gmail.com>
2025-12-19 00:32:20 +0800

    Add chart generation logic

---
+++ chart_generator.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+
+def update_pie_chart(axis, data):
+    """
+    更新圓餅圖
+
+    axis: Tkinter 傳進來的 Matplotlib 座標軸
+    data: 從 JSON 讀出來的 list
+    """
+    axis.clear()
+    if not data:
+        axis.set_title("尚無資料")
+        return
+
+    # 整理數據
+    sums = {}
+    for item in data:
+        category = item['category']
+        amount = float(item['amount'])
+        sums[category] = sums.get(category, 0) + amount
+
+    # 繪製圖表
+    labels = list(sums.keys())
+    values = list(sums.values())
+    
+    # 設定字體
+    plt.rcParams['font.sans-serif'] = ['Microsoft JhengHei'] 
+    
+    axis.pie(values, labels=labels, autopct='%1.1f%%', startangle=140)
+    axis.set_title("支出分類圓餅圖")

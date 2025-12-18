56c220a77c23bbcd4cecb446f3ca091a3ecafced
neon shark <tony50401@gmail.com>
2025-12-19 00:31:21 +0800

    Add input form to GUI

--- main_gui.py
+++ main_gui.py
@@ -6,6 +6,8 @@ class ExpenseApp:
     def __init__(self, root):
         self.root = root
         self.root.title("記帳工具")
+        self.amount_entry = tk.Entry(root)
+        self.btn = tk.Button(root, text="存檔", command=self.handle_submit)
 
 if __name__ == "__main__":
     root = tk.Tk()
